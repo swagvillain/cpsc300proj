6f4baada9c86460d6b1b04c68c904c17c28bdb99
Fred Brumovsky <freddiebrumovsky21@gmail.com>
2024-11-27 15:51:05 -0800

    basic pause menu (no buttons)

--- src/godot/cpsc300_projectv1/pause.gd
+++ src/godot/cpsc300_projectv1/pause.gd
@@ -1,11 +1,15 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		var new_pause_state = not get_tree().paused
+		get_tree().paused = new_pause_state
+		visible = new_pause_state
