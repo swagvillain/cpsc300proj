bdb0eaa11a3fdbe53d7262bb3c910a4d534c9f98
Fred Brumovsky <freddiebrumovsky21@gmail.com>
2024-11-27 17:34:52 -0800

    finished pause menu

--- src/godot/cpsc300_projectv1/pause.gd
+++ src/godot/cpsc300_projectv1/pause.gd
@@ -8,8 +8,23 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 	
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause"):
+func _input(pause_pressed: InputEvent) -> void:
+	if pause_pressed.is_action_pressed("pause"):
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = new_pause_state
 		visible = new_pause_state
+		if new_pause_state:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			
+
+
+func _on_regenerate_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+	
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
